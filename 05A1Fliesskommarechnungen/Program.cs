eaf0d7ce6f6ec9c517daee62a8c1e21a7f55d11a
frank paulsen <frobnic+ita@gmail.com>
2019-08-01 12:16:53 +0200

    Projektdateien hinzufügen.

---
+++ 05A1Fliesskommarechnungen/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05A1Fliesskommarechnungen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
