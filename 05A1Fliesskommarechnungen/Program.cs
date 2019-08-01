e29ef677894e462af9f8e6d1caa0315da4707ee0
frank paulsen <frobnic+ita@gmail.com>
2019-08-01 13:35:07 +0200

    added pause before calculation

--- 05A1Fliesskommarechnungen/Program.cs
+++ 05A1Fliesskommarechnungen/Program.cs
@@ -14,6 +14,8 @@ namespace _05A1Fliesskommarechnungen
 
             Console.WriteLine("f = {0:F2}  g = {1:F2}", f, g);
             Console.WriteLine();
+            Console.WriteLine("pause...");
+            System.Threading.Thread.Sleep(5000);
             Console.WriteLine("Summe     f+g {0,10:F4}", f + g);
             Console.WriteLine("Differenz f-g {0,10:F4}", f - g); 
             Console.WriteLine("Produkt   f*g {0,10:F4}", f * g);
