33dc25b5064b013d24e878f4e497889a640667f1
frank paulsen <frobnic+ita@gmail.com>
2019-08-01 12:29:28 +0200

    added formatstring

--- 05A1Fliesskommarechnungen/Program.cs
+++ 05A1Fliesskommarechnungen/Program.cs
@@ -10,6 +10,14 @@ namespace _05A1Fliesskommarechnungen
     {
         static void Main(string[] args)
         {
+            float f = 3.0f, g = 3.5f;
+
+            Console.WriteLine("f = {0:F2}  g = {1:F2}", f, g);
+            Console.WriteLine();
+            Console.WriteLine("Summe     f+g {0,10:F4}", f + g);
+            Console.WriteLine("Differenz f-g {0,10:F4}", f - g); 
+            Console.WriteLine("Produkt   f*g {0,10:F4}", f * g);
+            Console.WriteLine("Quotient  f/g {0,10:F4}", f / g);
         }
     }
 }
